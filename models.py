2e50259400173b23af969d69eacfcda6b41a0c48
Raka Maulana R. <mrrakaid@gmail.com>
2025-04-29 15:38:23 +0700

    Update for HW 2
    Some modification in python line in class

--- models.py
+++ models.py
@@ -8,20 +8,34 @@ class Restaurant(db.Model):
     __tablename__ = 'restaurant'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    street_address = Column(String(50))
+    street_address = Column(String(250)) #menambahkan karakter string dari 50 ke 250
     description = Column(String(250))
 
     def __str__(self):
         return self.name
 
+# class Review(db.Model): --the original code tetap di keep untuk cek perbedaannya
+#     __tablename__ = 'review'
+#     id = Column(Integer, primary_key=True)
+#     restaurant_id = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
+#     user_name = Column(String(30))
+#     rating = Column(Integer)
+#     review_text = Column(String(500))
+#     review_date = Column(DateTime) 
+
 class Review(db.Model):
     __tablename__ = 'review'
     id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))  # diganti dari restaurant
     user_name = Column(String(30))
     rating = Column(Integer)
     review_text = Column(String(500))
     review_date = Column(DateTime)
+
+    @validates('rating')
+    def validate_rating(self, key, value):
+        assert value is None or (1 <= value <= 5)
+        return value
 
     @validates('rating')
     def validate_rating(self, key, value):
